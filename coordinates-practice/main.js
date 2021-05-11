5467e907bf00428f5f5b95a2506129e3464f150e
Yunsu <kenatman0723@gmail.com>
2021-05-11 13:21:39 +0900

    coordinates perfomance improvement

--- coordinates-practice/main.js
+++ coordinates-practice/main.js
@@ -2,26 +2,28 @@ const targetImage = document.querySelector(".target");
 const targetText = document.querySelector(".target__text");
 const vertical = document.querySelector(".vertical");
 const horizontal = document.querySelector(".horizontal");
+const getRect = targetImage.getBoundingClientRect();
+const rectWidth = getRect.width / 2;
+const rectHeight = getRect.height / 2;
 
 function getTarget(e) {
   const x = e.clientX;
   const y = e.clientY;
 
-  targetImage.style.position = `absolute`;
-  targetImage.style.left = `${x - 60}px`;
-  targetImage.style.top = `${y - 60}px`;
+  targetImage.style.transform = `translate(${x - rectWidth}px,${
+    y - rectHeight
+  }px)`;
 
   targetText.innerHTML = `${x}px, ${y}px`;
-  targetText.style.position = `absolute`;
-  targetText.style.left = `${x + 60}px`;
-  targetText.style.top = `${y + 60}px`;
+  targetText.style.transform = `translate(${x}px,${y}px)`;
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
+  vertical.style.transform = `translateX(${x}px)`;
+  horizontal.style.transform = `translateY(${y}px)`;
 }
 
 function init() {
   window.addEventListener("mousemove", getTarget);
+  console.log(getRect);
 }
 
 init();
